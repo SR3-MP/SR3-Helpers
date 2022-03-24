179c3da1e7c746de4f82e9f9d95122ef4f6324fb
K3rhos <30273537+K3rhos@users.noreply.github.com>
2022-03-24 15:47:03 +0100

    Create Enums.h

---
+++ Miscellaneous/Enums.h
@@ -0,0 +1,61 @@
+enum class ControlAction
+{
+	Weapons_Wheel = 0,
+	Reload = 2,
+	Attack = 7,
+	Secondary_Attack = 8,
+	Aim = 10,
+	Knee_Kick = 11,
+	Jump = 13,
+	Sprint = 18,
+	Crouch = 25,
+	Weapons_Wheel_Vehicle = 36,
+	Accelerate = 40,
+	Decelerate = 41,
+	Handbrake = 42,
+	Horn = 45,
+	Nitro = 47,
+	Secondary_Attack_Vehicle = 69,
+	Use = 82,
+	Recruit_Follower = 83,
+	Pause_Menu = 85,
+	Open_Phone = 86,
+	Previous_Weapon = 88,
+	Next_Weapon,
+	Weapon_Slot_1,
+	Weapon_Slot_2,
+	Weapon_Slot_3,
+	Weapon_Slot_4,
+	Weapon_Slot_5,
+	Weapon_Slot_6,
+	Weapon_Slot_7,
+	Weapon_Slot_8,
+	Next_Grenade = 102,
+};
+
+
+
+enum class ControllerButton
+{
+	A,
+	B,
+	X,
+	Y,
+
+	LB,
+	RB,
+
+	LT,
+	RT,
+
+	DPAD_RIGHT,
+	DPAD_UP,
+	DPAD_LEFT,
+	DPAD_DOWN,
+
+	START,
+	BACK,
+
+	LS,
+	RS
+};
